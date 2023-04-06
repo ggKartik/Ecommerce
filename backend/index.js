589f2af06b07007a423dd0bdcac64d0ab7947bf0
Kartik <kartikjoshi1821028@gmail.com>
2023-04-06 11:44:24 +0530

    d

--- backend/index.js
+++ backend/index.js
@@ -12,35 +12,34 @@ dotenv.config({ path: "config.env" });
 
 app.use(cors());
 
-//config
-// const connectDB = require("./database/db");
-// const cloudinary = require("cloudinary");
-//config
+// config
+const connectDB = require("./database/db");
+const cloudinary = require("cloudinary");
 
-//connecting database
-// connectDB();
+// connecting database
+connectDB();
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET,
-// });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(fileUpload());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// //import route
-// const productRouter = require("./routes/productRoute");
-// const userRouter = require("./routes/userRouter");
-// const orderRouter = require("./routes/orderRoute");
-// const paymentRouter = require("./routes/paymentRoute");
+//import route
+const productRouter = require("./routes/productRoute");
+const userRouter = require("./routes/userRouter");
+const orderRouter = require("./routes/orderRoute");
+const paymentRouter = require("./routes/paymentRoute");
 
 // if (process.env.NODE_ENV === "development") {
-app.get("/", (req, res) => {
-  res.send("API is running.....");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running.....");
+// });
 // } else {
 //   app.use(express.static(path.join(__dirname, "../frontend/build")));
 //   //   app.get("*", (req, res) => {
@@ -48,13 +47,24 @@ app.get("/", (req, res) => {
 //   //   });
 // }
 
-// app.use("/api/v1", productRouter);
-// app.use("/api/v1", userRouter);
-// app.use("/api/v1", orderRouter);
-// app.use("/api/v1", paymentRouter);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// //middleware for error
-// app.use(errorMiddlware);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    };
+});
+
+app.use("/api/v1", productRouter);
+app.use("/api/v1", userRouter);
+app.use("/api/v1", orderRouter);
+app.use("/api/v1", paymentRouter);
+
+//middleware for error
+app.use(errorMiddlware);
 
 app.listen(process.env.PORT || 4500, () => {
   console.log(
